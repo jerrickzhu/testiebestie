79f76ec4ffeb55b56855a66a19a589b4748135a3
Jerrick Zhu <jerrickzhu@fb.com>
2021-08-04 16:53:21 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import FacebookLogin from "react-facebook-login";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [email, setEmail] = useState("");
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    if (response.accessToken) {
+      setLogin(true);
+      setEmail(response.email);
+    } else {
+      setLogin(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, this is my app. Welcome, friend.</h1>
+      {!login && (
+        <FacebookLogin
+          appId="49102200679461"
+          autoLoad={false}
+          fields="name, email, picture"
+          callback={responseFacebook}
+        />
+      )}
+      {login && <h2>huzzah! it worked! this is your email: {email}</h2>}
     </div>
   );
 }
